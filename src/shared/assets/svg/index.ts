e784b74a0da0decb3d27c31aead9c6f01a3b03fa
Nguyễn Lê Anh Tuấn <nguyenleanhtuan2708@gmail.com>
2022-01-27 23:05:15 +0700

    processing

---
+++ src/shared/assets/svg/index.ts
@@ -0,0 +1,5 @@
+export const TrangChu = require("./TrangChu.svg");
+export const QuanLyVe = require("./QuanLyVe.svg");
+export const DoiSoatVe = require("./DoiSoatVe.svg");
+export const CaiDat = require("./CaiDat.svg")
+
